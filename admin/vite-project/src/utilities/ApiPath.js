71af788ce18a4b5831c366991a02c0e3827a0f94
pranayAkarapu <pranayakarapu@gmail.com>
2025-10-17 18:12:01 +0530

    changes in apipath

--- admin/vite-project/src/utilities/ApiPath.js
+++ admin/vite-project/src/utilities/ApiPath.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API = "http://localhost:4000";
+export const API = import.meta.env.VITE_BACKEND_URL;
 
 export const API_URL = axios.create({
     baseURL:API
